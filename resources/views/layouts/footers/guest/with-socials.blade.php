f562eacc739ec3a8f360d5c1304e8b13ef3820ce
GetsemaniSal <getsemanisalazar7@gmail.com>
2021-12-22 23:01:53 -0600

    Proyecto Subido

---
+++ resources/views/layouts/footers/guest/with-socials.blade.php
@@ -0,0 +1,31 @@
+  <footer class="footer pb-5">
+      <div class="container">
+         
+              </div>
+              <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
+                  <a href="https://www.instagram.com/paraxute_music_studio/" target="_blank"
+                      class="text-secondary me-xl-4 me-4">
+                      <span class="text-lg fab fa-instagram"></span>
+                  </a>
+                  
+                  <a href="https://www.facebook.com/Paraxute/" target="_blank" class="text-secondary me-xl-4 me-4">
+                      <span class="text-lg fab fa-github"></span>
+                  </a>
+                  <a href="https://www.youtube.com/channel/UCp0q_qf4g59KVHKx1CkJaZw" target="_blank"
+                      class="text-secondary me-xl-4 me-4">
+                      <span class="text-lg fab fa-pinterest"></span>
+                  </a>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-8 mx-auto text-center mt-1">
+                  <p class="mb-0 text-secondary">
+                      Copyright ©Paraxute  · Music Lessons & Instruction School {{ now()->year }}   <a style="color: #252f40;"
+                          class="font-weight-bold ml-1" target="_blank"> </a> &amp; <a style="color: #252f40;"
+                           class="font-weight-bold ml-1" target="_blank"></a>
+              </div>
+              </p>
+          </div>
+      </div>
+      </div>
+  </footer>

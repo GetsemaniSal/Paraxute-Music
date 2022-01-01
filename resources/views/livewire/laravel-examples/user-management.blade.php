7c1d2d287f58c8e80e5167cae91a06e05404c67c
GetsemaniSal <getsemanisalazar7@gmail.com>
2022-01-01 00:32:37 -0600

    Est[etico

--- resources/views/livewire/laravel-examples/user-management.blade.php
+++ resources/views/livewire/laravel-examples/user-management.blade.php
@@ -1,13 +1,5 @@
 <div>
-    <div class="alert alert-secondary mx-4" role="alert">
-        <span class="text-white"><strong>Add, Edit, Delete features are not functional!</strong> This is a
-            <strong>PRO</strong> feature!
-            Click <strong><a href="https://demos.creative-tim.com/soft-ui-dashboard-laravel-pro/dashboard-default" target="_blank"
-                    class="text-white">here</a></strong>
-            to see the PRO
-            product!</span>
-    </div>
-
+   
     <div class="row">
         <div class="col-12">
             <div class="card mb-4 mx-4">

95222f355715938aa61da0a5cc0f5c684412dd71
GetsemaniSal <getsemanisalazar7@gmail.com>
2022-01-02 21:01:43 -0600

    Hasta aquí funciona

--- resources/views/Usuario/index.blade.php
+++ resources/views/Usuario/index.blade.php
@@ -1,42 +1,163 @@
-@extends('livewire.plantillabase')
-@section('contenido')
+<x-layouts.base>
+    {{-- If the user is authenticated --}}
+    @auth()
+        {{-- If the user is authenticated on the static sign up or the sign up page --}}
+        @if (in_array(request()->route()->getName(),['static-sign-up', 'sign-up'],))
+            @include('layouts.navbars.guest.sign-up')
+      
+            @include('layouts.footers.guest.with-socials')
+            {{-- If the user is authenticated on the static sign in or the login page --}}
+        @elseif (in_array(request()->route()->getName(),['sign-in', 'login'],))
+            @include('layouts.navbars.guest.login')
+      
+            @include('layouts.footers.guest.description')
+        @elseif (in_array(request()->route()->getName(),['profile', 'my-profile'],))
+            @include('layouts.navbars.auth.sidebar')
+            <div class="main-content position-relative bg-gray-100">
+                @include('layouts.navbars.auth.nav-profile')
+                <div>
+              
+                    @include('layouts.footers.auth.footer')
+                </div>
+            </div>
+            @include('components.plugins.fixed-plugin')
+        @else
+            @include('layouts.navbars.auth.sidebar')
+            @include('layouts.navbars.auth.nav')
+            @include('components.plugins.fixed-plugin')
+      
+            <main>
+                <div class="container-fluid">
+                    <div class="row">
+                        @include('layouts.footers.auth.footer')
+                    </div>
+                </div>
+            </main>
+        @endif
+    @endauth
+
+    {{-- If the user is not authenticated (if the user is a guest) --}}
+    @guest
+        {{-- If the user is on the login page --}}
+        @if (!auth()->check() && in_array(request()->route()->getName(),['login'],))
+            @include('layouts.navbars.guest.login')
+      
+            <div class="mt-5">
+                @include('layouts.footers.guest.with-socials')
+            </div>
+
+            {{-- If the user is on the sign up page --}}
+        @elseif (!auth()->check() && in_array(request()->route()->getName(),['sign-up'],))
+            <div>
+                @include('layouts.navbars.guest.sign-up')
+          
+                @include('layouts.footers.guest.with-socials')
+            </div>
+        @endif
+    @endguest
+
+</x-layouts.base>
+
+
 <!-- <div class="shadow-lg p-3 mb-5 bg-white rounded"><h3>Contenido de INDEX</h3></div> -->
-<a href="usuarios/create" class="btn btn-primary">CREAR</a>
+
+<div>
+    <div class="row">
+        <div class="col-12">
+            <div class="card mb-4 mx-4">
+                <div class="card-header pb-0">
+                    <div class="d-flex flex-row justify-content-between">
+                        <div>
+                            <h5 class="mb-0">Todos los Usuarios</h5>
+                        </div>
+                        <a href="usuarios/create" class="btn bg-gradient-primary btn-sm mb-0" type="button">+&nbsp; Nuevo Usuario</a>
+                    </div>
+                </div>
+                <div class="card-body px-0 pt-0 pb-2">
+                    <div class="table-responsive p-0">
+                        <table class="table align-items-center mb-0">
+                            <thead>
+                                <tr>
+                                     <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                       ID
+                                    </th>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                       Nombre
+                                    </th>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                        Correo
+                                    </th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Roll
+                                    </th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                       Contraseña
+                                    </th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                       teléfono
+                                    </th>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                       Acciones
+                                    </th>
+                                    
+                                </tr>
+                                            </thead>
+                            <tbody>
+                            @foreach ($usuarios as $usuario)
+                                <tr>
+                                    <td class="ps-4">
+                                        <p class="text-xs font-weight-bold mb-0">{{$usuario->id}}</p>
+                                    </td>
+                                    <td>
+                                        <div>
+                                        <p class="text-xs font-weight-bold mb-0">{{$usuario->name}}</p>
+                                        </div>
+                                    </td>
+                                    <td class="text-center">
+                                        <p class="text-xs font-weight-bold mb-0">{{$usuario->email}}</p>
+                                    </td>
+                                    <td class="text-center">
+                                        <p class="text-xs font-weight-bold mb-0">{{$usuario->role}}</p>
+                                    </td>
+                                    <td class="text-center">
+                                        <p class="text-xs font-weight-bold mb-0">{{$usuario->password}}</p>
+                                    </td>
+                                    <td class="text-center">
+                                        <span class="text-secondary text-xs font-weight-bold">{{$usuario->phone}}</span>
+                                    </td>
+                                    <td class="text-center">
+                                      <form action="{{ route('usuarios.destroy',$usuario->id) }}" method="POST">
+                                        <a href="/usuarios/{{$usuario->id}}/edit" class="mx-3" data-bs-toggle="tooltip"
+                                            data-bs-original-title="Edit user">
+                                            @csrf
+                                            @method('DELETE')
+                                            <i class="fas fa-user-edit text-secondary"></i>
+                                        </a>
+                                        <span>
+                                            <i class="cursor-pointer fas fa-trash text-secondary"></i>
+                                        </span>
+                                      </form>
+                                    </td>
+           
+        </td>   
+
+              
 
 
-<table class="table table-dark table-striped mt-4">
-  <thead>
-    <tr>
-      <th scope="col">id</th>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Role</th>
-      <th scope="col">password</th>
-      <th scope="col">phone</th>
-      <th scope="col">Acciones</th>
+                                    
+                                </tr>
+                               
+                                
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
 
-    </tr>
-  </thead>
-  <tbody>    
-    @foreach ($usuarios as $usuario)
-    <tr>
-        <td>{{$usuario->id}}</td>
-        <td>{{$usuario->name}}</td>
-        <td>{{$usuario->email}}</td>
-        <td>{{$usuario->role}}</td>
-        <td>{{$usuario->password}}</td>
-        <td>{{$usuario->phone}}</td>
-        <td>
-         <form action="{{ route('usuarios.destroy',$usuario->id) }}" method="POST">
-          <a href="/usuarios/{{$usuario->id}}/edit" class="btn btn-info">Editar</a>         
-              @csrf
-              @method('DELETE')
-          <button type="submit" class="btn btn-danger">Delete</button>
-         </form>          
-        </td>        
-    </tr>
-    @endforeach
-  </tbody>
-</table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-@endsection
+</div>   
+ 
